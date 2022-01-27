81c8165ca3bfd71970ad3e8022b6d831dbb356f5
Guimarães Neto <guimaraesneto@gmail.com>
2022-01-27 17:16:02 -0300

    Implemented config-server of Cambio-Service and Book-Service services

--- book-service/src/main/java/br/com/guimaraes/controller/BookController.java
+++ book-service/src/main/java/br/com/guimaraes/controller/BookController.java
@@ -35,7 +35,7 @@ public class BookController {
         var cambio = proxy.getCambio(book.getPrice(), "USD", currency);
 
         var port = environment.getProperty("local.server.port");
-        book.setEnvironment("Port: "+port);
+        book.setEnvironment("Book port: "+port+ " :: Cambio port" +cambio.getEnvironment() );
         book.setPrice(cambio.getConversionFactor());
         return book;
     }
